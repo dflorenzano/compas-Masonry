a5be6a3e6717f485d233460767e29484af9010c2
tomvanmele <tom.v.mele@gmail.com>
2025-01-20 00:36:27 +0100

    upgrade viewer

--- src/compas_masonry/viewers/blockmodelviewer.py
+++ src/compas_masonry/viewers/blockmodelviewer.py
@@ -1,278 +1,216 @@
 from compas.colors import Color
 from compas.datastructures import Mesh
 from compas.geometry import Line
-from compas.itertools import remap_values
 from compas_viewer import Viewer
-from compas_viewer.components import Button
-from compas_viewer.components import Slider
+from compas_viewer.components.lineedit import LineEdit
 from compas_viewer.scene import GroupObject
-from compas_viewer.scene import ViewerSceneObject
 
 from compas_masonry.elements import BlockElement
 from compas_masonry.elements import BlockMesh
 from compas_masonry.models import BlockModel
 
 
-def toggle_supports():
+def scale_compression(widget, value):
     viewer = BlockModelViewer()
-    if viewer.supports:
-        obj: ViewerSceneObject
-        for obj in viewer.supports.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def toggle_blocks():
-    viewer = BlockModelViewer()
-    if viewer.blocks:
-        obj: ViewerSceneObject
-        for obj in viewer.blocks.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def toggle_blockfaces():
-    viewer = BlockModelViewer()
-    if viewer.blocks:
-        obj: ViewerSceneObject
-        for obj in viewer.blocks.descendants:
-            obj.show_faces = not obj.show_faces
-    viewer.renderer.update()
-
-
-def toggle_interfaces():
-    viewer = BlockModelViewer()
-    if viewer.interfaces:
-        obj: ViewerSceneObject
-        for obj in viewer.interfaces.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def toggle_compression():
-    viewer = BlockModelViewer()
-    if viewer.compressionforces:
-        obj: ViewerSceneObject
-        for obj in viewer.compressionforces.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def toggle_tension():
-    viewer = BlockModelViewer()
-    if viewer.tensionforces:
-        obj: ViewerSceneObject
-        for obj in viewer.tensionforces.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def toggle_friction():
-    viewer = BlockModelViewer()
-    if viewer.frictionforces:
-        obj: ViewerSceneObject
-        for obj in viewer.frictionforces.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def toggle_resultants():
-    viewer = BlockModelViewer()
-    if viewer.resultantforces:
-        obj: ViewerSceneObject
-        for obj in viewer.resultantforces.descendants:
-            obj.show = not obj.show
-    viewer.renderer.update()
-
-
-def scale_compression(value):
-    if value <= 50:
-        values = list(range(1, 51))
-        values = remap_values(values, target_min=1, target_max=100)
-        scale = values[value - 1] / 100
-    else:
-        value = value - 50
-        values = list(range(0, 50))
-        values = remap_values(values, target_min=1, target_max=100)
-        scale = values[value - 1]
-
-    viewer = BlockModelViewer()
+    viewer.scale_compression = value
     if viewer.compressionforces:
         for obj, line in zip(viewer.compressionforces.descendants, viewer._compressionforces):
-            obj.geometry.start = line.midpoint - line.vector * 0.5 * scale
-            obj.geometry.end = line.midpoint + line.vector * 0.5 * scale
-            obj.update()
+            point = line.midpoint
+            vector = line.vector * 0.5 * value
+            obj.geometry.start = point - vector
+            obj.geometry.end = point + vector
+            obj.init()  # it is annoying that this is necessary
+    viewer.renderer.update()
+
+
+def scale_resultant(widget, value):
+    viewer = BlockModelViewer()
+    viewer.scale_resultant = value
+    if viewer.resultantforces:
+        for obj, line in zip(viewer.resultantforces.descendants, viewer._resultants):
+            point = line.midpoint
+            vector = line.vector * 0.5 * value
+            obj.geometry.start = point - vector
+            obj.geometry.end = point + vector
+            obj.init()  # it is annoying that this is necessary
     viewer.renderer.update()
 
 
 class BlockModelViewer(Viewer):
-    def __init__(self, **kwargs):
+    def __init__(self, blockmodel, **kwargs):
         super().__init__(**kwargs)
+
+        self.model: BlockModel = blockmodel
+
+        self._compressionforces: list[Line] = None
+        self._resultants: list[Line] = None
+
         self.supports: GroupObject = None
         self.blocks: GroupObject = None
         self.interfaces: GroupObject = None
-        self._compressionforces: list[Line] = None
         self.compressionforces: GroupObject = None
         self.tensionforces: GroupObject = None
         self.frictionforces: GroupObject = None
         self.resultantforces: GroupObject = None
 
         self.scale_compression = 1.0
+        self.scale_tension = 1.0
+        self.scale_friction = 1.0
+        self.scale_resultant = 1.0
+
+        self.color_block: Color = Color(0.9, 0.9, 0.9)
+        self.color_support: Color = Color.red()
+        self.color_interface: Color = Color.cyan()
+
+        self.show_blockfaces: bool = True
+        self.show_interfaces: bool = False
+        self.show_contactforces: bool = False
 
         self.ui.sidedock.show = True
-        self.ui.sidedock.add(Button(text="Toggle Supports", action=toggle_supports))
-        self.ui.sidedock.add(Button(text="Toggle Blocks", action=toggle_blocks))
-        self.ui.sidedock.add(Button(text="Toggle Block Faces", action=toggle_blockfaces))
-        self.ui.sidedock.add(Button(text="Toggle Interfaces", action=toggle_interfaces))
-        self.ui.sidedock.add(Button(text="Toggle Compression", action=toggle_compression))
-        self.ui.sidedock.add(Button(text="Toggle Tension", action=toggle_tension))
-        self.ui.sidedock.add(Button(text="Toggle Friction", action=toggle_friction))
-        self.ui.sidedock.add(Button(text="Toggle Resultants", action=toggle_resultants))
 
-        self.ui.sidedock.add(
-            Slider(
-                action=scale_compression,
-                min_val=1,
-                max_val=100,
-                starting_val=self.scale_compression * 50,
-                step=1,
-                title="Scale Compression",
-            )
-        )
+        self.ui.sidedock.add(LineEdit(str(self.scale_compression), "Scale Compression", action=scale_compression))
+        self.ui.sidedock.add(LineEdit(str(self.scale_resultant), "Scale Resultants", action=scale_resultant))
 
-    def add_blockmodel(
-        self,
-        blockmodel: BlockModel,
-        show_blockfaces: bool = True,
-        show_interfaces: bool = False,
-        show_contactforces: bool = False,
-        scale_compression: float = 1.0,
-        scale_friction: float = 1.0,
-        scale_tension: float = 1.0,
-        scale_resultant: float = 1.0,
-        color_block: Color = Color(0.9, 0.9, 0.9),
-        color_support: Color = Color.red(),
-        color_interface: Color = Color.green(),
-    ):
-        # add blocks and supports
+    # =============================================================================
+    # Show overwrite
+    # =============================================================================
 
+    def show(self):
+        self.init_blockmodel()
+        super().show()
+
+    # =============================================================================
+    # Model init
+    # =============================================================================
+
+    def init_blockmodel(self):
+        self.init_blocks()
+        self.init_interfaces()
+        self.init_forces()
+
+    # =============================================================================
+    # Blocks init
+    # =============================================================================
+
+    def init_blocks(self):
         supports: list[BlockMesh] = []
         blocks: list[BlockMesh] = []
 
-        for element in blockmodel.elements():
+        for element in self.model.elements():
             element: BlockElement
-
             if element.is_support:
-                supports.append(
-                    (
-                        element.modelgeometry,
-                        {
-                            "name": f"Support_{len(supports)}",
-                            "show_points": False,
-                            "show_faces": True,
-                            "facecolor": color_support,
-                            "linecolor": color_support.contrast,
-                        },
-                    )
-                )
+                supports.append((element.modelgeometry, {"name": f"Support_{len(supports)}"}))
             else:
-                blocks.append(
-                    (
-                        element.modelgeometry,
-                        {
-                            "name": f"Block_{len(blocks)}",
-                            "show_points": False,
-                            "show_faces": show_blockfaces,
-                            "facecolor": color_block,
-                            "linecolor": color_block.contrast,
-                        },
-                    )
-                )
+                blocks.append((element.modelgeometry, {"name": f"Block_{len(blocks)}"}))
 
         self.supports = self.scene.add(
             supports,
             name="Supports",
+            show_points=False,
+            show_faces=True,
+            facecolor=self.color_support,
+            linecolor=self.color_support.contrast,
         )
         self.blocks = self.scene.add(
             blocks,
             name="Blocks",
+            show_points=False,
+            show_faces=True,
+            facecolor=self.color_block,
+            linecolor=self.color_block.contrast,
         )
 
-        # add interfaces and interface forces
+    # =============================================================================
+    # Interfaces init
+    # =============================================================================
 
+    def init_interfaces(self):
         interfaces: list[Mesh] = []
-        # self._compressionforces: list[Line] = []
-        # tensionforces: list[Line] = []
-        # frictionforces: list[Line] = []
-        # resultantforces: list[Line] = []
 
-        for contact in blockmodel.contacts():
+        for contact in self.model.contacts():
             interfaces.append(contact.polygon.to_mesh())
-
-            # self._compressionforces += contact.compressionforces
-            # tensionforces += contact.tensionforces
-            # frictionforces += contact.frictionforces
-            # resultantforces += contact.resultantforce
 
         self.interfaces = self.scene.add(
             interfaces,
             name="Interfaces",
             show_points=False,
-            facecolor=color_interface,
-            linecolor=color_interface.contrast,
+            facecolor=self.color_interface,
+            linecolor=self.color_interface.contrast,
         )
 
-        # if scale_compression != 1.0:
-        #     self.scale_compression = scale_compression
-        #     for line in self._compressionforces:
-        #         if line.length:
-        #             line.start = line.midpoint - line.vector * 0.5 * scale_compression
-        #             line.end = line.midpoint + line.vector * 0.5 * scale_compression
+    # =============================================================================
+    # Forces init
+    # =============================================================================
 
-        # if scale_tension != 1.0:
-        #     for line in tensionforces:
-        #         if line.length:
-        #             line.start = line.midpoint - line.vector * 0.5 * scale_tension
-        #             line.end = line.midpoint + line.vector * 0.5 * scale_tension
+    def init_forces(self):
+        compressionforces: list[Line] = []
+        tensionforces: list[Line] = []
+        frictionforces: list[Line] = []
+        resultantforces: list[Line] = []
 
-        # if scale_friction != 1.0:
-        #     for line in frictionforces:
-        #         if line.length:
-        #             line.start = line.midpoint - line.vector * 0.5 * scale_friction
-        #             line.end = line.midpoint + line.vector * 0.5 * scale_friction
+        for contact in self.model.contacts():
+            compressionforces += contact.compressionforces
+            tensionforces += contact.tensionforces
+            frictionforces += contact.frictionforces
+            resultantforces += contact.resultantforce
 
-        # if scale_resultant != 1.0:
-        #     for line in resultantforces:
-        #         if line.length:
-        #             line.start = line.midpoint - line.vector * 0.5 * scale_resultant
-        #             line.end = line.midpoint + line.vector * 0.5 * scale_resultant
+        # keep a copy of the original forces
+        self._compressionforces = [line.copy() for line in compressionforces]
+        self._resultants = [line.copy() for line in resultantforces]
 
-        # if show_contactforces:
-        #     self.compressionforces = self.scene.add(
-        #         self._compressionforces,
-        #         name="Compression",
-        #         linewidth=3,
-        #         linecolor=Color.blue(),
-        #         show_points=False,
-        #     )
-        #     self.tensionforces = self.scene.add(
-        #         tensionforces,
-        #         name="Tension",
-        #         linewidth=5,
-        #         linecolor=Color.red(),
-        #         show_points=False,
-        #     )
-        #     self.frictionforces = self.scene.add(
-        #         frictionforces,
-        #         name="Friction",
-        #         linewidth=3,
-        #         linecolor=Color.cyan(),
-        #         show_points=False,
-        #     )
-        #     self.resultantforces = self.scene.add(
-        #         resultantforces,
-        #         name="Resultants",
-        #         linewidth=5,
-        #         linecolor=Color.green(),
-        #         show_points=False,
-        #     )
+        # apply scale if relevant
+
+        if self.scale_compression != 1.0:
+            for line in compressionforces:
+                if line.length:
+                    line.start = line.midpoint - line.vector * 0.5 * self.scale_compression
+                    line.end = line.midpoint + line.vector * 0.5 * self.scale_compression
+
+        if self.scale_tension != 1.0:
+            for line in tensionforces:
+                if line.length:
+                    line.start = line.midpoint - line.vector * 0.5 * self.scale_tension
+                    line.end = line.midpoint + line.vector * 0.5 * self.scale_tension
+
+        if self.scale_friction != 1.0:
+            for line in frictionforces:
+                if line.length:
+                    line.start = line.midpoint - line.vector * 0.5 * self.scale_friction
+                    line.end = line.midpoint + line.vector * 0.5 * self.scale_friction
+
+        if self.scale_resultant != 1.0:
+            for line in resultantforces:
+                if line.length:
+                    line.start = line.midpoint - line.vector * 0.5 * self.scale_resultant
+                    line.end = line.midpoint + line.vector * 0.5 * self.scale_resultant
+
+        # create groups
+
+        self.compressionforces = self.scene.add(
+            compressionforces,
+            name="Compression",
+            linewidth=3,
+            linecolor=Color.blue(),
+            show_points=False,
+        )
+        self.tensionforces = self.scene.add(
+            tensionforces,
+            name="Tension",
+            linewidth=5,
+            linecolor=Color.red(),
+            show_points=False,
+        )
+        self.frictionforces = self.scene.add(
+            frictionforces,
+            name="Friction",
+            linewidth=3,
+            linecolor=Color.cyan(),
+            show_points=False,
+        )
+        self.resultantforces = self.scene.add(
+            resultantforces,
+            name="Resultants",
+            linewidth=5,
+            linecolor=Color.green(),
+            show_points=False,
+        )
