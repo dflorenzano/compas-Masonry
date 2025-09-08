132cbf364b54ecf7b29d246764007a5d77113bcb
tomvanmele <tom.v.mele@gmail.com>
2025-09-08 22:52:12 +0200

    remove selfweight

--- src/compas_masonry/settings.py
+++ src/compas_masonry/settings.py
@@ -7,9 +7,8 @@ from compas_session.settings import Settings
 class FormDiagramSettings(BaseModel):
     show_reactions: bool = Field(default=True, title="Show Reactions")
     show_residuals: bool = Field(default=False, title="Show Residuals")
-    show_pipes: bool = Field(default=True, title="Show Pipes")
+    show_pipes: bool = Field(default=False, title="Show Pipes")
     show_loads: bool = Field(default=False, title="Show Loads")
-    show_selfweight: bool = Field(default=False, title="Show Selfweight")
     show_bounds: bool = Field(default=True, title="Show Bounds")
     show_cracks: bool = Field(default=False, title="Show Cracks")
 
@@ -17,7 +16,6 @@ class FormDiagramSettings(BaseModel):
     scale_residuals: float = Field(default=1.0, ge=1e-6, le=1e3, title="Scale Residuals")
     scale_pipes: float = Field(default=0.01, ge=1e-6, le=1e3, title="Scale Pipes")
     scale_loads: float = Field(default=1.0, ge=1e-6, le=1e3, title="Scale Loads")
-    scale_selfweight: float = Field(default=1.0, ge=1e-6, le=1e3, title="Scale Selfweight")
 
     tol_vectors: float = Field(default=1e-3, ge=1e-6, le=1e3, title="Tolerance Vectors")
     tol_pipes: float = Field(default=1e-2, ge=1e-6, le=1e3, title="Tolerance Pipes")
