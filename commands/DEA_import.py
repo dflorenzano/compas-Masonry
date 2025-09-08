80d4055032343cd6c4e52096a9a33d82309d9040
tomvanmele <tom.v.mele@gmail.com>
2025-09-08 22:50:11 +0200

    import export stubs

---
+++ commands/DEA_import.py
@@ -0,0 +1,18 @@
+#! python3
+# venv: brg-csd
+# r: compas_masonry
+
+
+from compas_masonry.session import MasonrySession as Session
+
+
+def RunCommand():
+    session = Session()
+
+
+# =============================================================================
+# Run as main
+# =============================================================================
+
+if __name__ == "__main__":
+    RunCommand()
