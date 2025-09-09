6cd9e551e4e294a868c2097c20d2039140b613e0
tomvanmele <tom.v.mele@gmail.com>
2025-09-09 13:42:17 +0200

    undo/redo stubs

---
+++ commands/Masonry_undo.py
@@ -0,0 +1,18 @@
+#! python3
+# venv: brg-csd
+# r: compas_masonry>=0.2.3
+
+
+from compas_rui.feedback import warn
+
+
+def RunCommand():
+    warn("Not available yet.")
+
+
+# =============================================================================
+# Run as main
+# =============================================================================
+
+if __name__ == "__main__":
+    RunCommand()
