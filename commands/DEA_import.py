61ac6ecccc8b2c60b437a1d70748de773273df2c
tomvanmele <tom.v.mele@gmail.com>
2025-09-09 12:53:03 +0200

    update commands

--- commands/DEA_import.py
+++ commands/DEA_import.py
@@ -1,6 +1,6 @@
 #! python3
 # venv: brg-csd
-# r: compas_masonry>=0.2.2
+# r: compas_masonry>=0.2.3
 
 
 from compas_masonry.session import MasonrySession as Session
--- commands/TNA_analysis.py
+++ commands/TNA_analysis.py
@@ -1,6 +1,6 @@
 #! python3
 # venv: brg-csd
-# r: compas_masonry>=0.2.2
+# r: compas_masonry>=0.2.3
 
 import ast
 
